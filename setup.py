45759fbc0a78535acf2841b993d2a87305f070d8
Kim Minho <minokim82@gmail.com>
2026-06-20 12:05:59 +0900

    feat: add setup.py and configure console entrypoint for kiwoom command

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kiwoom-cli",
+    version="0.1.0",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=[
+        "requests",
+        "click",
+    ],
+    entry_points={
+        "console_scripts": [
+            "kiwoom=kiwoom.cli:main",
+        ],
+    },
+)
